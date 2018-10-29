cf3ff6a24f5e22136d43898736e77cf030ab1f84
huanfuan <haunchen@163.com>
2018-10-30 02:01:39 +0800

    初始化aboutActivity

---
+++ app/src/main/java/com/huan/wanandroid_huan/ui/about/AboutActivity.java
@@ -0,0 +1,29 @@
+package com.huan.wanandroid_huan.ui.about;
+
+
+import com.huan.wanandroid_huan.R;
+import com.huan.wanandroid_huan.base.BaseActivity;
+
+public class AboutActivity extends BaseActivity {
+
+
+    @Override
+    protected int getLayoutId() {
+        return R.layout.activity_about;
+    }
+
+    @Override
+    protected void initView() {
+
+    }
+
+    @Override
+    protected void initData() {
+
+    }
+
+    @Override
+    public void shwoNoNet() {
+
+    }
+}
