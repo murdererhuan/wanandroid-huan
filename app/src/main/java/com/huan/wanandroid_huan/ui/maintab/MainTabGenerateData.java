e772167753588da5d8bdb9073478bb00f5afb080
huanfuan <haunchen@163.com>
2018-10-30 00:10:48 +0800

    初始四个button

--- app/src/main/java/com/huan/wanandroid_huan/ui/maintab/MainTabGenerateData.java
+++ app/src/main/java/com/huan/wanandroid_huan/ui/maintab/MainTabGenerateData.java
@@ -1,6 +1,10 @@
 package com.huan.wanandroid_huan.ui.maintab;
 
+import com.huan.wanandroid_huan.R;
+
 public class MainTabGenerateData {
 
+    public static final int[] tabResNormal = new int[]{R.drawable.tab_home_selector, R.drawable.tab_discovery_selector, R.drawable.tab_attention_selector, R.drawable.tab_profile_selector};
+    public static final int[] tabResPresse=new int[]{R.mipmap.ic_tab_strip_icon_feed_selected, R.mipmap.ic_tab_strip_icon_category_selected, R.mipmap.ic_tab_strip_icon_pgc_selected, R.mipmap.ic_tab_strip_icon_profile_selected};
 
 }
